14e0484a64878d3e37d3a89990074544fcb18333
tunan1994 <tunan@mail.ru>
2022-11-08 00:32:09 +0300

    HW2

--- HW2/Task4.java
+++ HW2/Task4.java
@@ -14,20 +14,15 @@ public class Task4 {
         System.out.println("Исходный массив: ");
         printArray(array);
         for (int left = 0; left < array.length; left++) {
-            // Вытаскиваем значение элемента
             int value = array[left];
-            // Перемещаемся по элементам, которые перед вытащенным элементом
             int i = left - 1;
             for (; i >= 0; i--) {
-                // Если вытащили значение меньшее — передвигаем больший элемент дальше
                 if (value < array[i]) {
                     array[i + 1] = array[i];
                 } else {
-                    // Если вытащенный элемент больше — останавливаемся
                     break;
                 }
             }
-            // В освободившееся место вставляем вытащенное значение
             array[i + 1] = value;
         }
         printArray(array);
